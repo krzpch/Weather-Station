2498ad44f1650108ca33f2e7b57ccc033d6c6345
krzpch <48431032+krzpch@users.noreply.github.com>
2021-01-11 18:59:28 +0100

    New functionality
    Added history function

--- Arduino_Code/Weather_station/Weather_station.ino
+++ Arduino_Code/Weather_station/Weather_station.ino
@@ -1,9 +1,9 @@
 #include <TimeLib.h> // Lib for time sync
 
-#include <Wire.h>
+#include <Wire.h> // Libs for I2C
 #include <LiquidCrystal_I2C.h> 
 
-#include <Adafruit_Sensor.h>
+#include <Adafruit_Sensor.h> // Libs for DHT 22 sensor
 #include <DHT.h>
 #include <DHT_U.h>
 
@@ -12,18 +12,28 @@
 
 #define TIME_HEADER     "SYNC"    // Header tag for serial time sync message
 #define DATA_HEADER     "DATA"    // Header tag for ordering temperature and humidity
-#define BACKLIGHT_ON    "BLON"    // Header tag for turning on LCD backlight
-#define BACKLIGHT_OFF   "BLOF"    // Header tag for turning off LCD backlight
+#define HISTORY_HEADER  "HIST"    // Header tag for ordering history
+
+#define HOLDED_HISTORY 24         // Number of holded history cells
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
+sensors_event_t event; // DHT event
 LiquidCrystal_I2C lcd(0x27,16,2);
 uint32_t delayMS;
 
+struct history_cell {
+  float h_temperature = 0.0f;
+  float h_humidity = 0.0f;
+  String h_time = " - ";
+};
+
 String receivedData = "";
 float Temperature, Humidity;
-uint8_t LCD_change = 0;
+struct history_cell history[HOLDED_HISTORY];
+int history_num = 0;
+int prev_time = 0;
 
-sensors_event_t event; // DHT event
+int LCD_change = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -49,6 +59,20 @@ void setup() {
 } // end of setup
 
 void loop() {  
+  if (history_num > HOLDED_HISTORY - 1){
+    history_num = 0;
+  }
+  if (minute() != prev_time) {
+    dht.temperature().getEvent(&event);
+    history[history_num].h_temperature = event.temperature;
+    dht.humidity().getEvent(&event);
+    history[history_num].h_humidity = event.relative_humidity;
+    history[history_num].h_time = ((String)hour() + ":" +  (String)(minute()/10) + (String)(minute()%10) + " " + (String)day() + "." + (String)(month()/10) + (String)(month()%10));
+
+    prev_time = minute();
+    history_num ++;
+  }
+  
   if (LCD_change == 0){  // LCD print temperature and humidity
     dht.temperature().getEvent(&event);
     Temperature = event.temperature;
@@ -69,7 +93,7 @@ void loop() {
     lcd.setCursor(0,1);
     temp = ((String)day() + "." + (String)(month()/10) + (String)(month()%10) + "." + (String)year() + "          ");
     lcd.print(temp);
-  } 
+  }
   
   if(Serial.available()>0)
   {
@@ -111,18 +135,25 @@ void checkMessageForTag(String text) {
     processSyncMessage(text);
   }
   else if (check_tag == DATA_HEADER){ // ordering data transfer
-    if (analogRead(A0) >= 500)
+    if (analogRead(A0) >= 500) // data won't be send if there is no connection
       Return_message();
-  } 
-  else if (check_tag == BACKLIGHT_ON){
-    lcd.backlight();
   }
-  else if (check_tag == BACKLIGHT_OFF){
-    lcd.noBacklight();
-  } 
+  else if (check_tag == HISTORY_HEADER){ // ordering data transfer
+    if (analogRead(A0) >= 500) // data won't be send if there is no connection
+      send_history();
+  }
 } //end of void checkMessageForTag()
 
-String today() {
+void send_history() {
+  for (int i = 0; i < HOLDED_HISTORY ; i++) {
+    String temp = ("Temp: " + String(history[(history_num + i) % HOLDED_HISTORY].h_temperature) + " , Hum: " + 
+    String(history[(history_num + i) % HOLDED_HISTORY].h_humidity) + " Time: " +  String(history[(history_num + i) % HOLDED_HISTORY].h_time));
+    Serial.println(temp);
+    delay(100);
+  }
+} //end of void history()
+
+String today() { 
   String week[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
   return week[weekday()];
 } // end of String today()
